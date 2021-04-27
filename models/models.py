baf890ef17bb45b6bfb4746b7f177971c61a3253
janesh <janeshlakshman7@gmail.com>
2021-04-27 15:07:47 +0530

    1st commit

---
+++ models/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from datetime import date
+
+
+class new(models.Model):
+     _name = 'new.new'
+     _description = 'new.new'
+      
+     name = fields.Char('Name')
+     date = fields.Date(string='Date')
+     state = fields.Char('State')
+
+     def cnfrm(self):
+         self.date=date.today()
+         self.state="SUCCESS"
+
+
+     
